2e6c3db1ae8cdffaa22e19c2898e45e521eaf7df
Nyaz Jaff <>
2025-03-21 23:07:04 +0000

    use test repo

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/rails/info/properties' , to: "rails/info#properties"
+  get '/rails/info/routes'     , to: "rails/info#routes"
+  get '/rails/info'            , to: "rails/info#index"
+  get '/'                      , to: "rails/welcome#index"
 end
